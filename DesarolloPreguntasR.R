28776b8ee12197f3e78966c7c45313fd318cd00e
Jorgeignaciorami <jorgeignaciorami@gmail.com>
2019-09-30 15:33:59 -0300

    Desarollo pregunta 1

---
+++ DesarolloPreguntasR.R
@@ -0,0 +1,57 @@
+listaDeNumeros <- list(2,5,6,2,1,5,6,10,11,20,15)
+Ejemplo:
+> listaDeNumeros <- list(2,5,6,2,1,5,6,10,11,20,15)
+> listaDeNumeros[5]
+[[1]]
+[1] 1
+
+Ejercicio 1:
+> listaDenumeros[0]
+Error: objeto 'listaDenumeros' no encontrado
+
+Sucede esto por que el numero 0 no esta dentro de la lista antes creada
+
+> listaDeNumeros[25]
+[[1]]
+NULL
+
+Este numero es mas largo que la lista y es mayor a los numeros que estan en esta, por lo tanto el programa no reconoce en que posición está.
+
+> listaDeNumeros[-25]
+[[1]]
+[1] 2
+
+[[2]]
+[1] 5
+
+[[3]]
+[1] 6
+
+[[4]]
+[1] 2
+
+[[5]]
+[1] 1
+
+[[6]]
+[1] 5
+
+[[7]]
+[1] 6
+
+[[8]]
+[1] 10
+
+[[9]]
+[1] 11
+
+[[10]]
+[1] 20
+
+[[11]]
+[1] 15
+
+
+
+
+
